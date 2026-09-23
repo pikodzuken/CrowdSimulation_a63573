dca2114834840ad3e2881783bddd807de0be0e79
Pi <a63573@alunos.ipb.pt>
2026-09-23 14:07:49 +0100

    Flocking passo 4

--- Assets/Flocking/Scripts/Flock.cs
+++ Assets/Flocking/Scripts/Flock.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Flock : MonoBehaviour
 {
     float speed;
+    bool turning = false;
 
     // Start is called before the first frame update
     void Start()
@@ -15,14 +16,32 @@ public class Flock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Random.Range(0, 100) < 10)
-        {
-            speed = Random.Range(FlockManager.FM.minSpeed, FlockManager.FM.maxSpeed);
-        }
+        Bounds b = new Bounds(FlockManager.FM.transform.position, FlockManager.FM.swimLimits * 2);
 
-        if (Random.Range(0, 100) < 10)
+        if (!b.Contains(transform.position))
         {
-            ApplyRules();
+            turning = true;
+        }
+        else
+            turning = false;
+
+        if (turning)
+        {
+            Vector3 direction = FlockManager.FM.transform.position - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), FlockManager.FM.rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+
+            if (Random.Range(0, 100) < 10)
+            {
+                speed = Random.Range(FlockManager.FM.minSpeed, FlockManager.FM.maxSpeed);
+            }
+
+            if (Random.Range(0, 100) < 10)
+            {
+                ApplyRules();
+            }
         }
 
         this.transform.Translate(0, 0, speed * Time.deltaTime);
